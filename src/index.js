5445990116f5f05036cd9725814579396b93b0ca
hiluluke <hiluluke@hotmail.com>
2018-02-04 17:07:34 +0800

    fetch post Data from graphQl

--- src/index.js
+++ src/index.js
@@ -3,22 +3,18 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
-// 1
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
-// 2
-const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
+const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cjd8jo4sz0bty0105jl712knh' })
 
-// 3
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache()
 })
 
-// 4
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
